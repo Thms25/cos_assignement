952167b61189e929bc3a23bfff7a551d78a7079f
Thomas Allen <thomasallenmartinho@gmail.com>
2024-06-12 10:07:53 +0200

    weekly goals started

--- src/components/home-content/WeeklyGoals.tsx
+++ src/components/home-content/WeeklyGoals.tsx
@@ -2,7 +2,12 @@ import Card from '../layouts/Card'
 
 export default function WeeklyGoals() {
   return (
-    <Card title="Weekly Goals">
+    <Card
+      title="Weekly Goals"
+      width="862.5px"
+      height="279px"
+      contentHeight="250px"
+    >
       <div>Here are your weekly goals</div>
     </Card>
   )
--- src/components/layouts/Card.tsx
+++ src/components/layouts/Card.tsx
@@ -3,6 +3,7 @@ import '../../styles/Card.scss'
 
 type CardProps = {
   children: React.ReactNode
+  button?: React.ReactNode
   title: string
   width?: string
   height?: string
@@ -13,22 +14,27 @@ type CardProps = {
 export default function Card({
   children,
   title,
-  width = '600px',
+  width,
   height,
   contentHeight,
   icon,
+  button,
 }: CardProps) {
   return (
     <section
       className="card"
       style={{
-        width,
+        width: '100%',
         height,
+        maxWidth: width,
       }}
     >
-      <div className="title">
-        <h4>{title}</h4>
-        {icon && <img src={icon} alt={title} />}
+      <div className="card-header">
+        <div className="card-title">
+          <h4>{title}</h4>
+          {icon && <img src={icon} alt={title} />}
+        </div>
+        {button && button}
       </div>
       <div
         className="card-content"
